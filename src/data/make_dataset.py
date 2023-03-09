ec48afdcd5f18cf75c9cbdc972ea46672794b62c
te1ord <ivanbashtovo2004@gmail.com>
2023-03-10 00:36:29 +0200

    add standartization

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -5,7 +5,7 @@ import numpy as np
 class AnonymizedDataset(Dataset):
     def __init__(self, path):
         self.data = np.loadtxt(path, delimiter=',', skiprows=1, usecols=[1, 2], dtype=np.float32)
-
+        self.data = (self.data - self.data.mean(axis=0)) / np.std(self.data, axis=0)
 
     def __len__(self):
         return len(self.data)
